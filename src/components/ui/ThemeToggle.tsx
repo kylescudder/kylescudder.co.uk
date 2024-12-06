858e49253444fc2a54a94769e6d2ffb9a2279e73
Kyle Scudder <kyle@kylescudder.co.uk>
2024-12-06 10:29:00 +0000

    feat(icon): replaced lucide with tabler icons

--- src/components/ui/ThemeToggle.tsx
+++ src/components/ui/ThemeToggle.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Moon, Sun } from 'lucide-react'
+import { IconSun, IconMoon } from '@tabler/icons-react'
 
 import { Button } from '@/components/ui/button'
 import {
@@ -36,8 +36,8 @@ export function ModeToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <IconSun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <IconMoon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
