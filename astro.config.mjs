8758fc2a3099ed2cf6dce1a47a087522eff15211
Kyle Scudder <kyle@kylescudder.co.uk>
2025-01-31 07:47:53 +0000

    feat(astro): update astro to version 5.2.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,20 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
-import tailwind from '@astrojs/tailwind'
 
 import vercel from '@astrojs/vercel'
+
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.CI ? 'https://kylescudder.co.uk' : 'http://localhost:4321',
 
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [react()],
 
   output: 'server',
   adapter: vercel(),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
