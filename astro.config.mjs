d9899a6dea7dacc35475ddc3f13874b6e80b9865
Kyle Scudder <kyle@kylescudder.co.uk>
2025-02-21 06:46:02 +0000

    feat(netlify): added adapters

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import vercel from '@astrojs/vercel'
 
 import tailwindcss from '@tailwindcss/vite'
 
+import netlify from '@astrojs/netlify'
+
 // https://astro.build/config
 export default defineConfig({
   site: process.env.CI ? 'https://kylescudder.co.uk' : 'http://localhost:4321',
@@ -12,7 +14,7 @@ export default defineConfig({
   integrations: [react()],
 
   output: 'server',
-  adapter: vercel(),
+  adapter: netlify(),
 
   vite: {
     plugins: [tailwindcss()],
