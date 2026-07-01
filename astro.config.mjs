78c8d45b1092918abb51eb895b41c7102c107d85
Kyle Scudder <kyle@kylescudder.co.uk>
2026-07-01 19:37:40 +0100

    fix: build portfolio as static site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,11 @@ import react from '@astrojs/react'
 
 import tailwindcss from '@tailwindcss/vite'
 
-import netlify from '@astrojs/netlify'
-
 // https://astro.build/config
 export default defineConfig({
   site: process.env.CI ? 'https://kylescudder.co.uk' : 'http://localhost:4321',
 
   integrations: [react()],
-
-  output: 'server',
-  adapter: netlify({
-    edgeMiddleware: true,
-  }),
 
   vite: {
     plugins: [tailwindcss()],
